1c009d153a1881a11efac83f70843e526abc1674
scyngit <100818565+scyngit@users.noreply.github.com>
2025-07-01 16:42:01 +0800

    Update app.js

--- formBuilderTest/js/app.js
+++ formBuilderTest/js/app.js
@@ -10,8 +10,16 @@ window.addEventListener('touchmove', e => {
     const currentY = e.touches[0].clientY;
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
+    // 如果在顶部 + 向下滑动
     if (scrollTop === 0 && currentY > startY) {
-        e.preventDefault(); //阻止刷新
+        // 失去所有输入焦点（防止弹出输入法）
+
+        /* beautify ignore:start */
+        document.activeElement?.blur?.();
+        /* beautify ignore:end */
+
+        // 阻止浏览器下拉默认行为（刷新页面）
+        e.preventDefault();
     }
 }, {
     passive: false
